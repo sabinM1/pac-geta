8146a6bb6cb1cafa0db997e2a2ce13d4fe4ca3b5
edin citaku <edin.citaku@jetbrains.com>
2021-02-10 09:38:00 +0100

    modivied bewegungsfunktionen

--- tutorial/funktionen-1.js
+++ tutorial/funktionen-1.js
@@ -1,19 +1,19 @@
 function bewegeHoch(x, y, geschwindigkeit){
-    var neuesY = y - geschwindigkeit;
+    var neuesY = y ;
     return neuesY;
 }
 
 function bewegeRunter(x, y, geschwindigkeit){
-    var neuesY = y + geschwindigkeit;
+    var neuesY = y;
     return neuesY;
 }
 
 function bewegeRechts(x, y, geschwindigkeit){
-    var neuesX = x + geschwindigkeit;
+    var neuesX = x;
     return neuesX;
 }
 
 function bewegeLinks(x, y, geschwindigkeit){
-    var neuesX = x - geschwindigkeit;
+    var neuesX = x;
     return neuesX;
 }
